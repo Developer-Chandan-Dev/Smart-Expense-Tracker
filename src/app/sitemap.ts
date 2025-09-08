1eeaaa4ac0076f84e110ab5763ccc381572facb4
Chandan Dev <chandan.dev.pro@gmail.com>
2025-09-08 08:31:04 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://smart-expense-tracker.vercel.app'
+  const baseUrl = 'https://smart-expense-tracker2.vercel.app'
 
   return [
     {
